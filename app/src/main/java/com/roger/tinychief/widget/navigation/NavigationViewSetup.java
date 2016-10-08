9bb81ea3311edf3467fe5c27e5e768b703960ea6
Roger <roger22002988@hotmail.com>
2016-10-08 14:54:08 +0800

    改一堆亂取的變數名稱,刪掉多餘的function

--- app/src/main/java/com/roger/tinychief/widget/navigation/NavigationViewSetup.java
+++ app/src/main/java/com/roger/tinychief/widget/navigation/NavigationViewSetup.java
@@ -7,15 +7,13 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.roger.tinychief.R;
 import com.roger.tinychief.activity.CreateActivity;
-import com.roger.tinychief.activity.DetailActivity;
 import com.roger.tinychief.activity.LoginActivity;
 import com.roger.tinychief.activity.MainActivity;
 
@@ -41,12 +39,19 @@ public class NavigationViewSetup {
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(mActivity, mDrawerLayout, mToolbar, R.string.openDrawer, R.string.closeDrawer);
         //calling sync state is necessay or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
-        View mHeader=mNavigationView.getHeaderView(0);
-        Button headerLogin = (Button)mHeader.findViewById(R.id.headerLoginButton);
+        View header=mNavigationView.getHeaderView(0);
+        if(MainActivity.USER_NAME!=null) {
+            TextView name = (TextView) header.findViewById(R.id.txtview_name_header);
+            name.setText(MainActivity.USER_NAME);
+            Button button=(Button) header.findViewById(R.id.btn_login_header);
+            button.setVisibility(View.INVISIBLE);
+        }
+        Button headerLogin = (Button)header.findViewById(R.id.btn_login_header);
         headerLogin.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),LoginActivity.class);
+                mDrawerLayout.closeDrawers();
                 mActivity.startActivity(intent);
             }
 
@@ -59,27 +64,20 @@ public class NavigationViewSetup {
                 mDrawerLayout.closeDrawers();
                 switch (menuItem.getItemId()) {
                     case R.id.nav_item_hot:
-                        Toast.makeText(mActivity.getApplicationContext(), "nav_item_hot", Toast.LENGTH_SHORT).show();
                         jumpToActivity(mActivity, MainActivity.class);
                         return true;
                     case R.id.nav_item_love:
-                        Toast.makeText(mActivity.getApplicationContext(), "nav_item_love", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.nav_item_create:
-                        Toast.makeText(mActivity.getApplicationContext(), "nav_item_create", Toast.LENGTH_SHORT).show();
                         jumpToActivity(mActivity, CreateActivity.class);
                         return true;
                     case R.id.nav_item_calendar:
-                        Toast.makeText(mActivity.getApplicationContext(), "nav_item_calendar", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.nav_item_history:
-                        Toast.makeText(mActivity.getApplicationContext(), "nav_item_history", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.nav_item_setting:
-                        Toast.makeText(mActivity.getApplicationContext(), "nav_item_setting", Toast.LENGTH_SHORT).show();
                         return true;
                     default:
-                        Toast.makeText(mActivity.getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
                         return true;
                 }
             }

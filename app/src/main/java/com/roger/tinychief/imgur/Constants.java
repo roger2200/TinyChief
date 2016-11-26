0f5416f949aef037038098293045f60a5b04df00
Roger <roger22002988@hotmail.com>
2016-11-26 23:58:51 +0800

    更新imgur的token和修改字體大小

--- app/src/main/java/com/roger/tinychief/imgur/Constants.java
+++ app/src/main/java/com/roger/tinychief/imgur/Constants.java
@@ -2,7 +2,7 @@ package com.roger.tinychief.imgur;
 
 public class Constants {
     public static final boolean LOGGING = false;
-    public static final String MY_IMGUR_CLIENT_ID = "fe3935a2abe49cc6411232cdfbedc283dec159fa";
+    public static final String MY_IMGUR_CLIENT_ID = "6d61b29074e0de0fdd41af65cfbd157c939954cf";
 
     public static String getClientAuth() {
         return "Bearer " + MY_IMGUR_CLIENT_ID;
